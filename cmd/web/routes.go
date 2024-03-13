3290731d9999155fb67e869010bb78604da92741
klexq <daniilevgaliev@yandex.ru>
2024-03-13 11:08:11 +0300

    [FEATURES] +render.go +config +basic handler

---
+++ cmd/web/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"net/http"
+)
+
+func (app *application) routes() http.Handler {
+	r := chi.NewRouter()
+
+	r.Get("/virtual-terminal", app.VirtualTerminal)
+
+	return r
+}
